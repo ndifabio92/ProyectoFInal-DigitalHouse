8797b36110e58c2ec1b155304244131c86e3cae0
Nicolás Di Fabio <ndifabio@icebergsolutions.com>
2023-10-25 21:57:05 -0300

    commit temporal

---
+++ Front-End/src/components/admin/table/headCells/headTableAdmin.js
@@ -0,0 +1,20 @@
+export const headCells = [
+    {
+        id: "id",
+        numeric: false,
+        disablePadding: true,
+        label: "Id"
+    },
+    {
+        id: "name",
+        numeric: false,
+        disablePadding: false,
+        label: "Nombre"
+    },
+    {
+        id: "actions",
+        numeric: false,
+        disablePadding: false,
+        label: "Acciones"
+    }
+];
