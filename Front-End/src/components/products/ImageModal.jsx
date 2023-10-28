17a5302953be1840af905439a340c6272b4eaf6a
celu32 <celu_32@hotmail.com>
2023-10-28 11:51:27 -0300

    se corrigen detalles de  modal de imagenes

--- Front-End/src/components/products/ImageModal.jsx
+++ Front-End/src/components/products/ImageModal.jsx
@@ -1,28 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
 import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-
-
-
 
 
 
 const ImageModal = ({ images }) => {
-  // const images = ["../futbol1.png", "../futbol2.png", "../futbol3.png", "../futbol4.png", "../futbol5.png", "../tenis4.png", "../tenis1.png", "../tenis2.png", "../tenis3.png", "../tenis4.png", "../padel1.jpg", "../padel2.jpg", "../padel3.jpg", "../padel4.jpg", "../padel5.jpg", "../nat1.png", "../nat2.png", "../nat4.jpg", "../nat5.jpg", "../nata3.jpg"
-  // ]
-
+ 
   const [open, setOpen] = useState(false);
-
 
   const handleOpen = () => {
     setOpen(true);
@@ -86,7 +76,7 @@ const ImageModal = ({ images }) => {
         </IconButton>
 
         <DialogContent dividers={scroll === 'paper'} sx={{ padding: '30px' }}>
-          <DialogContentText
+          <Box
             id="scroll-dialog-description"
             ref={descriptionElementRef}
             tabIndex={-1}
@@ -104,7 +94,7 @@ const ImageModal = ({ images }) => {
                 ))
               }
             </ImageList>
-          </DialogContentText>
+          </Box>
         </DialogContent>
       </Dialog>
     </>
@@ -112,52 +102,3 @@ const ImageModal = ({ images }) => {
 }
 
 export default ImageModal
-
-
-
-
-/*
-
-return (
-    <>
-
-    <Box sx={{
-        textAlign:'left',
-        margin: '10px',
-        display:'flex',
-        justifyContent:'flex-end'
-      }}>
-      <Button 
-      variant="contained"
-      sx={{
-        padding: '10px',
-      }}
-      onClick={handleOpen}
-    >
-      Ver Mas
-    </Button>
-    </Box>
-    <div>
-      <Modal
-        keepMounted
-        open={open}
-        onClose={handleClose}
-      >
-        <Box sx={style}>
-          <ImageList variant="masonry" cols={3} gap={8}>
-            {images.map((image, imageIndex) => (
-              <ImageListItem key={imageIndex}>
-                <img
-                  src={image}
-                  loading="lazy"
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-        </Box>
-      </Modal>
-    </div>
-    </>
-  );
-}
-*/
--- Front-End/src/components/products/Images.jsx
+++ Front-End/src/components/products/Images.jsx
@@ -1,12 +1,10 @@
-import { Container, Grid, Button, Box, Modal, Typography } from '@mui/material';
+import { Container, Grid} from '@mui/material';
 import ImageModal from './ImageModal';
 
 const imagenPrinc = '../futbol1.png';
 const imagenes = ["../futbol2.png", "../futbol3.png", "../futbol4.png", "../futbol5.png"];
-
-
-
-
+const imagenes2 = ["../futbol1.png", "../futbol2.png", "../futbol3.png", "../futbol4.png", "../futbol5.png", "../tenis4.png", "../tenis1.png", "../tenis2.png", "../tenis3.png", "../tenis4.png", "../padel1.jpg", "../padel2.jpg", "../padel3.jpg", "../padel4.jpg", "../padel5.jpg", "../nat1.png", "../nat2.png", "../nat4.jpg", "../nat5.jpg", "../nata3.jpg"]
+ 
 
 const Images = () => {
   return (
@@ -36,7 +34,7 @@ const Images = () => {
           </Grid>
         </Grid>
       </Container>
-      <ImageModal images={imagenes} />
+      <ImageModal images={imagenes2} />
     </>
 
   );
