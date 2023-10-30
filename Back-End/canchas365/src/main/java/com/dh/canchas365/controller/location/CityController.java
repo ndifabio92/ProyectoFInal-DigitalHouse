834413162396e39a729b32af0f0d9a6a1966fd76
Sergio Aciar <aciarenator@gmail.com>
2023-10-29 21:39:50 -0300

    se agrega endpoint para listar ciudades

--- Back-End/canchas365/src/main/java/com/dh/canchas365/controller/location/CityController.java
+++ Back-End/canchas365/src/main/java/com/dh/canchas365/controller/location/CityController.java
@@ -1,15 +1,15 @@
 package com.dh.canchas365.controller.location;
 
+import com.dh.canchas365.dto.location.CityCreateDTO;
 import com.dh.canchas365.dto.location.CityDTO;
 import com.dh.canchas365.model.location.City;
 import com.dh.canchas365.service.location.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/city")
@@ -19,8 +19,13 @@ public class CityController {
     private CityService cityService;
 
     @PostMapping
-    public ResponseEntity<City> createCity(@RequestBody CityDTO dto){
+    public ResponseEntity<City> createCity(@RequestBody CityCreateDTO dto){
         return new ResponseEntity<City>(cityService.createCity(dto), HttpStatus.CREATED);
 
+    }
+
+    @GetMapping("/list")
+    public List<CityDTO> getAllCities(){
+        return cityService.getAllCities();
     }
 }
