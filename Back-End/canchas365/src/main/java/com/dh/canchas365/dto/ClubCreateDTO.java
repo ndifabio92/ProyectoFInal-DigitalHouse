73220ba63c01e3d4004fae93fec9e7928912cdf2
Sergio Aciar <aciarenator@gmail.com>
2023-10-29 21:09:26 -0300

    se agrega dto para creacion de club

---
+++ Back-End/canchas365/src/main/java/com/dh/canchas365/dto/ClubCreateDTO.java
@@ -0,0 +1,31 @@
+package com.dh.canchas365.dto;
+
+import com.dh.canchas365.dto.images.ImageDTO;
+import com.dh.canchas365.dto.location.AdressDTO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ClubCreateDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String phone_number;
+
+    private Boolean recommended;
+
+    private AdressDTO adress;
+
+    private Set<ImageDTO> images;
+}
