2914a00bed62dd54c5befca83ec8b6983f61a5f2
isebedio <isebedio@gmail.com>
2023-11-04 09:26:43 -0300

    se agrega avatar al header

--- Front-End/src/components/ui/header/Header.jsx
+++ Front-End/src/components/ui/header/Header.jsx
@@ -1,20 +1,27 @@
-import { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 
+const pages = ["Crear cuenta", "Iniciar sesión"];
+const settings = ["Perfil", "Cerrar sesión"];
 
-const pages = ['Crear cuenta', 'Iniciar sesión'];
+const user = {
+  name: "Lali",
+  surname: "Perez",
+};
 
 const Header = () => {
-  
+  const logo = "./logoNaranjaNeg.png";
   const [anchorElNav, setAnchorElNav] = useState(null);
 
   const handleOpenNavMenu = (event) => {
@@ -25,20 +32,27 @@ const Header = () => {
     setAnchorElNav(null);
   };
 
-  const logo = './logoNaranjaNeg.png'
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
-
     <AppBar position="fixed">
-      <Container maxWidth="xxl" sx={{
-        padding: '0px',
-        margin: '0px',
-        backgroundColor: '#1F2E7B',
-        height: '100px',
-
-      }}
+      <Container
+        maxWidth="xxl"
+        sx={{
+          padding: "0px",
+          margin: "0px",
+          backgroundColor: "#1F2E7B",
+          height: "100px",
+        }}
       >
-
         <Toolbar disableGutters>
           <Typography
             variant="h6"
@@ -46,31 +60,30 @@ const Header = () => {
             component="a"
             href="/"
             sx={{
-              display: 'flex',
-              fontFamily: 'monospace',
-              color: 'inherit',
+              display: "flex",
+              fontFamily: "monospace",
+              color: "inherit",
               fontSize: 12,
-              alignItems: 'end',
+              alignItems: "end",
             }}
           >
-            <img src={logo} style={{ height: '100px' }} />
+            <img src={logo} style={{ height: "100px" }} />
             <span>Qué Deporte jugamos hoy? </span>
           </Typography>
-
 
           <Box
             sx={{
               flexGrow: 1,
-              display: { xs: 'none', md: 'flex' },
-              mx: '50px',
-              justifyContent: 'flex-end',
+              display: { xs: "none", md: "flex" },
+              mx: "50px",
+              justifyContent: "flex-end",
             }}
           >
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
                 {page}
               </Button>
@@ -80,11 +93,10 @@ const Header = () => {
           <Box
             sx={{
               flexGrow: 1,
-              display: { xs: 'flex', md: 'none' },
-              justifyContent: 'flex-end',
+              display: { xs: "flex", md: "none" },
+              justifyContent: "flex-end",
             }}
           >
-
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -102,24 +114,53 @@ const Header = () => {
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
-                alignContent: 'rigth',
+                display: { xs: "block", md: "none" },
+                alignContent: "rigth",
               }}
             >
-              {
-                pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))
-              }
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
             </Menu>
           </Box>
 
-        </Toolbar>
+          {user ? (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar>{user.name[0] + user.surname[0]}</Avatar>
+                </IconButton>
+              </Tooltip>
 
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          ) : null}
+        </Toolbar>
       </Container>
     </AppBar>
   );
-}
+};
 export default Header;
