64f921301e81175df9a3cfe3719d9941b868e437
isebedio <isebedio@gmail.com>
2023-11-06 20:55:01 -0300

    Se agrega ruta a Navigation /signin, mas componente UserSignIn y FormUserSignIn

---
+++ Front-End/src/components/user/form 2/FormUserSignIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormUserSignIn = () => {
+  return (
+    <div>Hacer form user</div>
+  )
+}
+
+export default FormUserSignIn
---
+++ Front-End/src/pages/user/UserSignIn.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container } from "@mui/material";
+import FormUserSignIn from "../../components/user/form 2/FormUserSignIn";
+
+const UserSignIn = () => {
+  return (
+    <div>
+      <Container
+        maxWidth="xxl"
+        sx={{
+          backgroundColor: "#FFFFFF",
+          color: "#1F2E7B",
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "center",
+          gap: "10px",
+          mt: { xs: "50px", sm: "150px" }, // Ajuste del margen en dispositivos móviles
+          padding: "20px", // Ajuste del padding en dispositivos móviles
+        }}
+      >
+        <FormUserSignIn />
+      </Container>
+    </div>
+  );
+};
+
+export default UserSignIn;
