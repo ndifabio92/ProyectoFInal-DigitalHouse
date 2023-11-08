1c00d8bd41791195c92d2ab38bfb7d23d75d8fe3
Nicolás Di Fabio <ndifabio@icebergsolutions.com>
2023-11-07 22:19:53 -0300

    se agrega authorize en el header

---
+++ Back-End/canchas365/src/main/java/com/dh/canchas365/security/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.dh.canchas365.security;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SwaggerConfig {
+}
