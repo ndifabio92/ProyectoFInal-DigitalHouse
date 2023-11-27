c0a3ea7cbc2ec50747f152829cb74f45385ab9a5
Nicolás Di Fabio <ndifabio@icebergsolutions.com>
2023-11-27 20:17:42 -0300

    commit temporal

--- Front-End/src/components/ui/search/SearchBar.jsx
+++ Front-End/src/components/ui/search/SearchBar.jsx
@@ -1,36 +1,20 @@
 import Container from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 import { useForm } from '../../../hooks/useForm';
 import { useNavigate } from 'react-router-dom';
 import SelectInput from '../../selectinput/SelectInput';
 import Datepicker from '../../datepicker/Datepicker';
 import { ENDPOINTS } from '../../../constants/endpoints';
-import { METHODS } from '../../../constants/methods';
 import useFetchApi from '../../../hooks/useFetchApi';
-import useFetchDataApi from '../../../hooks/useFetchDataApi';
 
 
 const SearchBar = () => {
 
   const navigate = useNavigate();
-  const { data: categories, isLoading: isLoadingCategories, error: categoriesError} = useFetchApi(`${ENDPOINTS.CATEGORY}`);
-  const { data: cities, isLoading: isLoadingCity, error: cityError} = useFetchApi(`${ENDPOINTS.CITY}`);
+  const { data: categories } = useFetchApi(`${ENDPOINTS.CATEGORY}`);
+  const { data: cities } = useFetchApi(`${ENDPOINTS.CITY}`);
 
-  const { data, isLoading, error, fetchData } = useFetchDataApi();
-
-  const clubs = async () => {
-    try {
-      const result = await fetchData(ENDPOINTS.CLUB_SEARCH, METHODS.POST, ids);
-    } catch (error) {
-      console.log(error)
-    }
-  };
-
+  console.log(categories, cities)
   const { values, handleChange } = useForm({
     city: '',
     sport: '',
@@ -39,12 +23,12 @@ const SearchBar = () => {
   });
 
   const handleClick = () => {
-    
-    navigate(`/club/search`);
+    const queryParams = `city=${encodeURIComponent(values.city.id)}&sport=${encodeURIComponent(values.sport.id)}&date=${encodeURIComponent(values.date)}&time=${encodeURIComponent(values.time)}`;
+    navigate(`/club/search?${queryParams}`);
   };
 
   return (
-    <Container 
+    <Container
 
       sx={{
         color: '#FF914D',
@@ -53,50 +37,16 @@ const SearchBar = () => {
         flexWrap: 'wrap',
         gap: '40px',
         alignItems: 'center',
-        justifyContent:'center',
+        justifyContent: 'center',
         padding: '50px'
       }}
     >
-      <FormControl sx={{ m: 1, width: 300, display: "flex", gap: 5 }}>
-        <InputLabel>Seleccionar categoría</InputLabel>
-        <Select
-          handleChange={handleChange}
-          labelId="categorias"
-          input={<OutlinedInput label="Seleccionar categoría" />}
-        >
-          {categories?.map((category) => (
-            <MenuItem
-              key={category.id}
-              value={category.title}
-            >
-              {category.title}
-            </MenuItem>
-          ))}
-        </Select>
+      <SelectInput handleChange={handleChange} options={cities} name="city" />
+      <SelectInput handleChange={handleChange} options={categories} name="sport" />
+      <Datepicker handleChange={handleChange} name="date" type="DatePicker" />
+      <Datepicker handleChange={handleChange} name="time" type="DatePicker" />
 
-        <InputLabel>Seleccionar ciudad</InputLabel>
-        <Select
-          handleChange={handleChange}
-          labelId="ciudades"
-          input={<OutlinedInput label="Seleccionar ciudad" />}
-        >
-          {cities?.map((city) => (
-            <MenuItem
-              key={city.id}
-              value={city.name}
-            >
-              {city.name}
-            </MenuItem>
-          ))}
-        </Select>
-        <Datepicker handleChange={handleChange} name="date" type="DatePicker" />
-        <Datepicker handleChange={handleChange} name="time" type="DatePicker" />
-        <Button variant="contained" onClick={handleClick} type='submit'>Buscar Turno</Button>
-      </FormControl>
-      {/* <SelectInput handleChange={handleChange} options={cities} name="city" />
-      <SelectInput handleChange={handleChange} options={sports} name="sport" /> */}
-      {/* <Button variant="contained" type='submit'>Buscar Turno</Button> */}
-
+      <Button variant="contained" onClick={handleClick} type='submit'>Buscar Turno</Button>
     </Container >
 
   );
--- Front-End/src/pages/clubSearch/ClubSearch.jsx
+++ Front-End/src/pages/clubSearch/ClubSearch.jsx
@@ -5,36 +5,46 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { Container } from '@mui/material';
+import useFetchApi from '../../hooks/useFetchApi';
+import { ENDPOINTS } from '../../constants/endpoints';
+import { METHODS } from '../../constants/methods';
+import dayjs from 'dayjs';
 
 const ClubSearch = () => {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const filters = {
-        city: queryParams.get('city') || '',
-        sport: queryParams.get('sport') || '',
-        date: queryParams.get('date') || '',
-        time: queryParams.get('time') || '',
+        city: { id: +queryParams.get('city') },
+        sport: { id: +queryParams.get('sport') },
+        dateTime: `${dayjs(queryParams.get('date').$d).format('YYYY-MM-DDT')}${dayjs(queryParams.get('time')).format('HH:mm:ssZ[Z]')}`
     };
+
+
+    // const dateTime = `${dayjs(filters.date.$d).format('YYYY-MM-DDT')}${dayjs(filters.time).format('HH:mm:ssZ[Z]')}`
+    const { data, isLoading, error } = useFetchApi(ENDPOINTS.CLUB_SEARCH, METHODS.POST, filters);
+    console.log(data)
+    // console.log(filters)
+    console.log(filters)
 
     const [clubs, setClubs] = useState([]);
     const [noClubsFound, setNoClubsFound] = useState(false);
 
-    useEffect(() => {
-        const fetchFilteredClubs = async () => {
-        try {
-            const response = await axios.post('/club/search', filters);
-            setClubs(response.data);
-            setNoClubsFound(response.data.length === 0);
-        } catch (error) {
-            console.error('Error al obtener clubes filtrados:', error);
-        }
-        };
+    // useEffect(() => {
+    //     const fetchFilteredClubs = async () => {
+    //         try {
+    //             const response = await axios.post('/club/search', filters);
+    //             setClubs(response.data);
+    //             setNoClubsFound(response.data.length === 0);
+    //         } catch (error) {
+    //             console.error('Error al obtener clubes filtrados:', error);
+    //         }
+    //     };
 
-        fetchFilteredClubs();
-    }, [filters]);
+    //     fetchFilteredClubs();
+    // }, [filters]);
 
     return (
-        <Container 
+        <Container
             maxWidth="xl"
             sx={{
                 padding: '0',
@@ -47,31 +57,31 @@ const ClubSearch = () => {
             id='home'
         >
             <div style={{ padding: '20px', textAlign: 'center' }}>
-            <SearchBar />
-            <h1 style={{ margin: '20px 0', color: '#333' }}>Club Search Results</h1>
-            {noClubsFound ? (
-                <p style={{ color: '#FF4500', fontSize: '18px' }}>
-                No se han encontrado clubes que coincidan con lo que buscas.
-                </p>
-            ) : (
-                <div>
-                {clubs.map((club) => (
-                    <CardProducts
-                    key={club.id}
-                    name={club.name}
-                    tel={club.phone_number}
-                    city={
-                        club.address.street +
-                        " N° " +
-                        club.address.number +
-                        ", " +
-                        club.address.city.name
-                        }
-                        id={club.id}
-                    />
-                ))}
-                </div>
-            )}
+                {/* <SearchBar /> */}
+                <h1 style={{ margin: '20px 0', color: '#333' }}>Club Search Results</h1>
+                {noClubsFound ? (
+                    <p style={{ color: '#FF4500', fontSize: '18px' }}>
+                        No se han encontrado clubes que coincidan con lo que buscas.
+                    </p>
+                ) : (
+                    <div>
+                        {clubs.map((club) => (
+                            <CardProducts
+                                key={club.id}
+                                name={club.name}
+                                tel={club.phone_number}
+                                city={
+                                    club.address.street +
+                                    " N° " +
+                                    club.address.number +
+                                    ", " +
+                                    club.address.city.name
+                                }
+                                id={club.id}
+                            />
+                        ))}
+                    </div>
+                )}
             </div>
         </Container>
     );
